3233046eaf33ba93c382285182bec88bf2afa0a2
ramustha <ramusthanest@gmail.com>
2020-01-09 13:27:48 +0700

    change to javaee 8.0

---
+++ src/main/java/com/adiwisista/jsf/playground/ActiveJsf23Config.java
@@ -0,0 +1,9 @@
+package com.adiwisista.jsf.playground;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class ActiveJsf23Config {
+}
--- src/main/java/com/adiwisista/jsf/playground/Hello.java
+++ src/main/java/com/adiwisista/jsf/playground/Hello.java
@@ -2,14 +2,18 @@ package com.adiwisista.jsf.playground;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Named
 @RequestScoped
 public class Hello {
+    private static final Logger log = Logger.getAnonymousLogger();
     private String name;
     private String message;
 
     public void createMessage() {
+        log.log(Level.WARNING, "################ Warning");
         message = "Hello, " + name + "!";
     }
 
